bffd6ebcbe45a2bce69f03d6a136c9187001d650
chandratop <ccservices@protonmail.ch>
2019-06-29 11:39:13 +0530

    Changed paper location

---
+++ Corollary4.py
@@ -0,0 +1,132 @@
+
+# Corollary :
+# # If (jr = jr+1 = jr+2) then,
+# # ir+2 = jr+2 and
+# # Sr+1[jr+1] = Sr+2[ir+2] = Sr+2[jr+2] = 0
+
+
+# * Modules imported
+
+from random import shuffle
+
+
+# * Functions
+
+def nextarr(arr, i, j, N=16):
+    '''
+    This function used the RC4 algorithm to
+    create the next iteration of the array
+    based on the values of i and j.
+        {param} arr : The previous array
+        {param} i : The previous i
+        {param} j : The previous j
+        {param} N : Default parameter 16, for array length
+    '''
+
+    # algorithm
+    i = (i + 1) % N
+    j = (j + arr[i]) % N
+
+    # swap
+    arr[i], arr[j] = arr[j], arr[i]
+
+    # return requires tuple unpacking
+    return arr, i, j
+
+
+def check1(j_seq):
+    '''
+    In the sequence of j's if j(r) = j(r+1) = j(r+2),
+    then return True, else, return False.
+    r is the index.
+        {param} j_seq : Four consecutive values of j in a list
+    '''
+
+    if j_seq[0] == j_seq[1] == j_seq[2]:
+        return True
+    else:
+        return False
+
+def check2(i, j):
+    '''
+    If i(r+2) = j(r+2) it returns True,
+    else it returns False.
+    r is the index.
+        {param} i : i(r+2)
+        {param} j : j(r+2)
+    '''
+
+    return i == j
+
+def check3(S_seq, i, j_seq):
+    '''
+    If  Sr+1[jr+1] = Sr+2[ir+2] = Sr+2[jr+2] = 0 then,
+    it returns True, else it returns False.
+    r is the index.
+        {param} S_seq : Contains the last two iterations of arr
+        {param} i : i(r+2)
+        {param} j_seq : Used to take j(r+1) and j(r+2) values
+    '''
+
+    return S_seq[1][j_seq[1]] == S_seq[2][i] == S_seq[2][j_seq[2]] == 0
+
+
+# * Main
+
+theorem = True
+
+# Asks how many shuffles it needs to so the algorithm on
+number_of_shuffles = int(input("How many shuffles should it check : "))
+
+# Asks how many rounds to do for each shuffle
+# -2 since we do 2 rounds to create the initial j_sequence [line 46-50]
+number_of_rounds = int(input("How many times should each shuffle run : ")) - 2
+
+while number_of_shuffles > 0 and theorem is True:
+
+    # Initial values
+    arr = [x for x in range(16)]
+    shuffle(arr)
+    i, j = 0, 0
+
+    j_sequence = [j]
+    S_sequence = [arr]
+    i_2 = i # i(r+2)
+
+    # Pre-running for 2 more times to set
+    # j_sequence with first three values of j and
+    # S_sequence with first three values of arr
+    for run in range(2):
+        arr, i, j = nextarr(arr, i, j)
+        j_sequence.append(j)
+        S_sequence.append(arr)
+        i_2 = i
+
+    # To check if the Corollary is wrong at the start itself
+    # even though the j_sequence test is passeds (check1)
+    if check1(j_sequence) is True:
+        if check2(i_2, j_sequence[2]) and check3(S_sequence, i_2, j_sequence) is False:
+            print("Corollary 3 is wrong")
+            break
+
+    for round in range(number_of_rounds):
+
+        # get next
+        arr, i, j = nextarr(arr, i, j)
+
+        # update j_sequence and S_sequence
+        j_sequence[0], S_sequence[0] = j_sequence[1], S_sequence[1]
+        j_sequence[1], S_sequence[1] = j_sequence[2], S_sequence[2]
+        j_sequence[2], S_sequence[2] = j, arr
+
+        # To check if the Corollary is wrong
+        # even though the j_sequence test is passeds (check1)
+        if check1(j_sequence) is True:
+            if check2(i_2, j_sequence[2]) and check3(S_sequence, i_2, j_sequence) is False:
+                print("Corollary 3 is wrong")
+                break
+
+    number_of_shuffles -= 1
+
+if theorem is True:
+    print("Corollary hasn't been proved False")
--- README.md
+++ README.md
@@ -6,3 +6,9 @@
 * Under Prof. Subhamoy Maitra of ISI Kolkata
 
 ![alt text](https://www.isical.ac.in/~repro/wp-content/themes/isirepro/images/isi4_1.png "ISI Kolkata")
+
+## Research Paper
+
+This repository contains the code to test out the Theorems and
+Corollaries of the research paper. To get access to the paper,
+please [mail](mailto:chandratop.mail@gmail.com) me.
